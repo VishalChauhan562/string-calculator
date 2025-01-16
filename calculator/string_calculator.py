f1eebb9e3615fb7291e9d7edec241de33d2d5959
Vishal <vishal-personal@MacBook-Pro.local>
2025-01-16 19:51:14 +0530

    sixth test passed test_support_different_delimiter

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -2,6 +2,11 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+    if numbers.startswith("//"):
+        delimiter = numbers[2]
+        numbers = numbers[4:]  
+        numbers = numbers.replace(delimiter, ",")
+
     numbers = numbers.replace("\n",",")
     if "," in numbers:
         numbers_list = numbers.split(",")
