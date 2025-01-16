4f6be9e0ceb0fe1df652352a1c0c57293c5b1aa2
Vishal <vishal-personal@MacBook-Pro.local>
2025-01-16 19:29:42 +0530

    second case failed test_single_number_returns_that_number

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -2,3 +2,8 @@ from calculator.string_calculator import add
 
 def test_empty_string_returns_zero(): 
     assert add("") == 0
+
+
+def test_single_number_returns_that_number():
+    from calculator.string_calculator import add
+    assert add("1") == 1
