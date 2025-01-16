ed92ecbef90bba71be836acc975368fd46614138
Vishal <vishal-personal@MacBook-Pro.local>
2025-01-16 19:40:02 +0530

    forth test passed test_multiple_numbers_returns_their_sum

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -4,5 +4,5 @@ def add(numbers: str) -> int:
         return 0
     if "," in numbers:
         numbers_list = numbers.split(",")
-        return int(numbers_list[0]) + int(numbers_list[1])
+        return sum(int(num) for num in numbers_list)
     return int(numbers)
