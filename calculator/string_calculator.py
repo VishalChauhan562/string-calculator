99b20a806ee4982aa8ee8d85fd2ee7e079cddf46
Vishal <vishal-personal@MacBook-Pro.local>
2025-01-16 19:35:14 +0530

    third test passed test_two_numbers_returns_their_sum

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -2,4 +2,7 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+    if "," in numbers:
+        numbers_list = numbers.split(",")
+        return int(numbers_list[0]) + int(numbers_list[1])
     return int(numbers)
