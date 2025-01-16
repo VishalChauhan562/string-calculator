0ca94fca929073c12ae6ecec29d2917c4adbad71
Vishal <vishal-personal@MacBook-Pro.local>
2025-01-16 19:44:08 +0530

    sixth test failed test_support_different_delimiter

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -18,3 +18,7 @@ def test_multiple_numbers_returns_their_sum():
 
 def test_newlines_between_numbers():
     assert add("1\n2,3") == 6
+
+
+def test_support_different_delimiter():
+    assert add("//;\n1;2") == 3
