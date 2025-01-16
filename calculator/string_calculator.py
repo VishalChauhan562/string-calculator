b16cb271885abb75e5f1dfe360c04f3a584c61bd
Vishal <vishal-personal@MacBook-Pro.local>
2025-01-16 19:42:39 +0530

    fifth test passed test_newlines_between_numbers

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -2,6 +2,7 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+    numbers = numbers.replace("\n",",")
     if "," in numbers:
         numbers_list = numbers.split(",")
         return sum(int(num) for num in numbers_list)
