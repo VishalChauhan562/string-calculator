0a74bc89db2498208752382d0ffb20334ed26739
Vishal <vishal-personal@MacBook-Pro.local>
2025-01-16 19:57:03 +0530

    seventh test passed test_negative_numbers_throw_exception

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -10,5 +10,9 @@ def add(numbers: str) -> int:
     numbers = numbers.replace("\n",",")
     if "," in numbers:
         numbers_list = numbers.split(",")
-        return sum(int(num) for num in numbers_list)
+        numbers_int = [int(num) for num in numbers_list]
+        negative_nums = [num for num in numbers_int if num < 0 ]
+        if negative_nums:
+            raise ValueError(f"negatives not allowed: {', '.join(map(str, negative_nums))}")
+        return sum(numbers_int)
     return int(numbers)
